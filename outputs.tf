286f4219ece8196cebc8eb575c5914cadf188e7e
Brendan Smith <brendan@lincolnloop.com>
2024-09-25 10:10:36 -0400

    try to output the arn of the SNS topic

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@
 
 output "sns_topic_arn" {
   description = "The ARN of the SNS topic for cost anomaly alerts"
-  value       = module.aws_cost_anomaly_detector.cost_anomaly_updates.arn
+  value       = module.aws_cost_anomaly_detector.sns_arn
 }
